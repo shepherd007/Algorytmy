59e00b1a7c29549f00fba1979656893398785fc0
Grzegorz Suder <Greg@GregDesktop.(none)>
2012-04-26 18:23:15 +0200

    Init

---
+++ heapsort.cpp
@@ -0,0 +1,88 @@
+// Heapsort.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int A[] = {0,1,3,2,4,5};
+int heapSize = sizeof(A)/sizeof(A[0]);
+
+void heapify(int i)
+{
+    int left_index = 2*i;
+    int right_index = 2*i+1;
+
+    int largest = i;
+    int len = sizeof(A)/sizeof(A[0]);
+
+    if(left_index < heapSize &&
+       A[left_index] > A[i])
+    {
+        largest = left_index;
+    }
+
+    if(right_index < heapSize &&
+       A[right_index] > A[largest])
+    {
+        largest = right_index;
+    }
+
+    if(largest != i)
+    {
+        int tmp = A[i];
+        A[i] = A[largest];
+        A[largest] = tmp;
+
+        heapify(largest);
+    }
+}
+
+void buildHeap()
+{
+    int len = (sizeof(A)/sizeof(A[0]))/2 + 1;
+    heapSize = sizeof(A)/sizeof(A[0]);
+    for(int i=len; i>0; i--)
+    {
+        heapify(i);
+    }
+}
+
+void heapSort()
+{
+    int len = (sizeof(A)/sizeof(A[0]));
+
+    buildHeap();
+    for(int i=len-1; i>=2; i--)
+    {
+        int tmp = A[i];
+        A[i] = A[1];
+        A[1] = tmp;
+        heapSize--;
+        heapify(1);
+    }
+}
+
+void insert(int v)
+{
+    heapSize+=1;
+    int i=heapSize;
+
+    while(i>1 && A[i/2] < v)
+    {
+        int tmp = A[i];
+        A[i] = A[i/2];
+        A[i/2] = A[i];
+        i = i/2;
+    }
+
+    A[i] = v;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+   // heapSort();
+   // buildHeap();
+    //insert(9);
+    heapSort();
+	return 0;
+}
+
