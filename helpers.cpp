859aff8e889ace57ad875f4e2110e3e5500fdcc0
Grzegorz Suder <Greg@GregDesktop.(none)>
2012-05-06 13:22:45 +0200

    Uproszczenie kodu

--- helpers.cpp
+++ helpers.cpp
@@ -4,7 +4,10 @@ Example: swap(&tab[0], &tab[1])
 */
 void swap(int* A, int* B)
 {
-	*A = (*A) ^ (*B);
-	*B = (*B) ^ (*A);
-	*A = (*B) ^ (*A);
+	if(A != B)
+	{
+		*A ^= (*B);
+		*B ^= (*A);
+		*A ^= (*B);
+	}
 }
